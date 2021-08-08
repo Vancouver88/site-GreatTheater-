dad3dabb8c8081eb585140d1e05ddfe45350e55f
Vancouver <voron_pnevmatika@mail.ru>
2021-08-08 19:31:15 +0300

    Сайт Большого театра

---
+++ scripts/swiper.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var swiper = new Swiper(".mySwiper", {
+    //slidesPerView: 4, //при разрешении более 1410
+    //slidesPerView: 3, //при разрешении до 1210
+    //slidesPerView: 1.65, //при разрешении до 1060
+    slidesPerView: 1.2, //при разрешении до 750
+    spaceBetween: 0,
+    centeredSlides: false,
+});
